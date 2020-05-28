9ce4ad619c0f0dc50b572c095071c2a8fa34cc8d
田家正 <tjz0224@gmail.com>
2020-05-28 20:56:33 +0200

    learned how api works

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,46 @@
-// Global app controller
-import secret from './test';
+import axios from 'axios';
 
-console.log(`I' m !!! index.js, I know a secret which is ${secret}`);
+async function getRecipe(query){
+    try {
+        const res = await axios({
+            "method":"GET",
+            "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search",
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key":"6254e2e442msh734c64054f6c642p1b8196jsn949ae6b44e00",
+            "useQueryString":true
+            },"params":{
+            "number":"100",
+            "query": query
+            }
+            });
+        console.log(res.data.results);
+    } catch(error){
+        alert(error);
+    }
+}
+
+async function getWeather(query){
+    try {
+        const res = await axios({
+            "method":"GET",
+            "url":`https://dark-sky.p.rapidapi.com/${query[0]},${query[1]}`,
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"dark-sky.p.rapidapi.com",
+            "x-rapidapi-key":"6254e2e442msh734c64054f6c642p1b8196jsn949ae6b44e00",
+            "useQueryString":true
+            },"params":{
+            "lang":"en",
+            "units":"auto"
+            }
+            });
+        
+        console.log(res.data.daily.data);
+    } catch (error) {
+        alert(error);
+    }
+}
+getWeather([39.909058, 116.389181]);
+getRecipe('rice');
--- src/js/test.js
+++
@@ -1,3 +0,0 @@
-console.log('I\' m test.js');
-
-export default 'oh lalala';
