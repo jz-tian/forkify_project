ac4308cf4f9de1654660f0914cb843d34ffcaf5b
田家正 <tjz0224@gmail.com>
2020-06-04 17:31:31 +0200

    added serach single recipe for model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -52,3 +53,7 @@ const controlSearch = async () => {
     }
 }
 
+//recipe controller
+const r = new Recipe(489101);
+r.getRecipe();
+console.log(r);
